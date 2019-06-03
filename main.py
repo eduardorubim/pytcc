878c704828496d01eea046bc2b3804b72070d187
Eduardo Rubim <rubim.eduardo@gmail.com>
2019-06-03 15:58:21 -0300

    iniciando session client com credenciais do arquivo (gitignorado)

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import dialogflow
 from google.api_core.exceptions import InvalidArgument
+from google.oauth2 import service_account
 
 from tts import TTS
 from asr import ASR
 
-DIALOGFLOW_PROJECT_ID = 'newagent-7d078'
+DIALOGFLOW_PROJECT_ID = 'smart-home-1-6c30f'
 DIALOGFLOW_LANGUAGE_CODE = 'pt-BR'
-GOOGLE_APPLICATION_CREDENTIALS = 'newagent-7d078-65fdc9af8063.json'
+GOOGLE_APPLICATION_CREDENTIALS = service_account.Credentials.from_service_account_file('/credentials/smart-home-1-6c30f-b1024858ceb7.json')
 SESSION_ID = 'tcc-chatbot'
 
 text_to_be_analyzed = ASR.listen()
 
-session_client = dialogflow.SessionsClient()
+session_client = dialogflow.SessionsClient(credentials=GOOGLE_APPLICATION_CREDENTIALS)
 session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
 
 text_input = dialogflow.types.TextInput(text=text_to_be_analyzed, language_code=DIALOGFLOW_LANGUAGE_CODE)
