77b9a4c0a4bc40ca1558ad3c246276bb9c403642
rmyamguchi <rmyamguchi@gmail.com>
2019-08-27 19:13:30 -0300

    impelementacao dos metodos de acesso a memoria semantica

--- src/sm.py
+++ src/sm.py
@@ -6,9 +6,48 @@ class SemanticMemory:
         self.onto = get_ontology("file://pytcc/smarthome/smarthome.owl").load()
         self.indoor_places = self.onto.search(is_a = self.onto.IndoorPlace)
         self.outdoor_places = self.onto.search(is_a = self.onto.OutdoorPlace)
+        onto_path.append("pytcc/smarthome/")
+
+    """
+    Retorna o nome (String) dado um local (Place)
+    """
+    def getName(self, place):
+        return place.isNamed
+
+    """
+    Retorna a lista de dispositivos de um local (Place)
+    """
+    def getSmartDevices(self, place):
+        return place.hasSmartDevice
+
+    """
+    Retorna o estado do dispositivo (SmartDevice)
+    """
+    def getDeviceIsOn(self, smart_device):
+        return smart_device.isOn
+
+    """
+    Liga um dispositivo (SmartDevice)
+    """
+    def turnOn(self, smart_device):
+        smart_device.isOn = True
+        self.onto.save()
+
+    """
+    Desliga um dispositivo (SmartDevice)
+    """
+    def turnOff(self, smart_device):
+        smart_device.isOn = False
+        self.onto.save()    
+
 
 if __name__ == "__main__":
     sm = SemanticMemory()
-    smart_device = sm.indoor_places[1].hasSmartDevice
-    print(sm.indoor_places[1].isNamed)
-    print(smart_device[0].isOn)
+    for place in sm.indoor_places[1:]:
+        print (sm.getName(place))
+        smart_devices = sm.getSmartDevices(place)
+        for device in smart_devices:
+            print("  " + str(sm.getDeviceIsOn(device)))
+            
+    #sm.turnOn(sm.indoor_places[1].hasSmartDevice[0])
+    #print(sm.indoor_places[1].hasSmartDevice[0].isOn)
